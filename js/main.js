b50101257bb96bd147ff2c84c26a89184450451a
John Howie <jkhowie63@gmail.com>
2016-09-14 15:04:47 -0400

    updated to use === instead of ==

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(function(){
     var self = $(this);
     var winner = false;
     if (turn <= 8) {
-      if(self.text() == ''){
+      if(self.text() === ''){
         self.text(turnArray[turn++]);
         if (turn > 4){
           if(checkWinner()){
@@ -19,7 +19,7 @@ $(function(){
       }
     }
 
-    if(!winner && turn == 9){
+    if(!winner && turn === 9){
       alert('No One Wins Game Over!')
     }
   });
@@ -31,7 +31,7 @@ $(function(){
 
     self.find('tr').each(function (r, row){
       $(this).find('td').each(function (c, col){
-        if($(col).text() == player || $(col).text() == ''){
+        if($(col).text() === player || $(col).text() === ''){
           magicTable[r][c] = 0;
         }
       });
@@ -42,22 +42,22 @@ $(function(){
   function checkWinningPattern(magicTable){
     //check for winning row
     for(var r = 0; r < 3; r++){
-      if((magicTable[r][0] + magicTable[r][1] + magicTable[r][2]) == 15){
+      if((magicTable[r][0] + magicTable[r][1] + magicTable[r][2]) === 15){
         return true;
       }
     }
     //check for winning col
     for(var c = 0; c < 3; c++){
-      if((magicTable[0][c] + magicTable[1][c] + magicTable[2][c]) == 15){
+      if((magicTable[0][c] + magicTable[1][c] + magicTable[2][c]) === 15){
         return true;
       }
     }
     // check for winning top left to bottom right
-    if((magicTable[0][0] + magicTable[1][1] + magicTable[2][2]) == 15){
+    if((magicTable[0][0] + magicTable[1][1] + magicTable[2][2]) === 15){
       return true;
     }
     // check for winning bottol left to top right
-    if((magicTable[0][2] + magicTable[1][1] + magicTable[2][0]) == 15){
+    if((magicTable[0][2] + magicTable[1][1] + magicTable[2][0]) === 15){
       return true;
     }
     return false;
